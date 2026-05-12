09740de34a7e85ea9384ad1f1318abdceda8b52b
alan5454 <siuug17@gmail.com>
2026-05-11 17:12:17 -0700

    Alan: Cambio en los urls.py

--- Estadisticas/urls.py
+++ Estadisticas/urls.py
@@ -16,4 +16,9 @@ urlpatterns = [
     path('jugadores/nuevo/', views.jugador_crear, name='jugador_crear'),
     path('jugadores/<uuid:pk>/editar/', views.jugador_editar, name='jugador_editar'),
     path('jugadores/<uuid:pk>/eliminar/', views.jugador_eliminar, name='jugador_eliminar'),
+    # Carrito
+    path('carrito/', views.cart_detail, name='cart_detail'),
+    path('carrito/agregar/<uuid:equipo_id>/', views.add_to_cart, name='add_to_cart'),
+    path('carrito/eliminar/<uuid:item_id>/', views.remove_from_cart, name='remove_from_cart'),
+    path('carrito/actualizar/<uuid:item_id>/', views.update_cart_item, name='update_cart_item'),
 ]
